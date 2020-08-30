b21e813902d8749ac9ed11a8de5416cf46508119
Ta-ryo <yarot.wsaiaik@outlook.jp>
2020-08-30 18:35:24 +0900

    improve skill graph

---
+++ src/components/AboutSkillFrontend.vue
@@ -0,0 +1,53 @@
+<script>
+import { Bar } from 'vue-chartjs'
+export default {
+  extends: Bar,
+  data: function () {
+    return {
+      type: 'bar',
+      data: {
+        labels: ['HTML', 'CSS', 'SCSS', 'Bootstrap', 'JavaScript', 'jQuery', 'Vue.js'],
+        datasets: [
+          {
+            data: [80, 60, 70, 40, 60, 60, 70, 80, ''],
+            backgroundColor: [
+              '#fff',
+              '#fff',
+              '#fff',
+              '#fff',
+              '#fff',
+              '#fff',
+              '#fff'
+            ],
+            barPercentage: 0.2
+          }
+        ]
+      },
+      options: {
+        responsive: false,
+        legend: {
+          display: false
+        },
+        scales: {
+          xAxes: [{
+            display: true,
+            position: 'top',
+            ticks: {
+              min: 0,
+              max: 100,
+              fontSize: 14,
+              fontColor: '#fff'
+            }
+          }],
+          yAxes: [{
+            display: false
+          }]
+        }
+      }
+    }
+  },
+  mounted () {
+    this.renderChart(this.data, this.options)
+  }
+}
+</script>
