d58637184b6c04335f07372467a4d0716cffc492
Ta-ryo <yarot.wsaiaik@outlook.jp>
2020-09-09 05:06:26 +0900

    make profile responsive

---
+++ src/components/AboutSkillGrauhForSp.vue
@@ -0,0 +1,65 @@
+<script>
+import { HorizontalBar } from 'vue-chartjs'
+export default {
+  extends: HorizontalBar,
+  data: function () {
+    return {
+      type: 'horizontalBar',
+      data: {
+        labels: ['HTML', 'CSS', 'SCSS', 'Bootstrap', 'JavaScript', 'jQuery', 'Vue.js', 'PHP', 'Ruby', 'rails', 'SQL', 'WordPress', 'Firebase', 'Github'],
+        datasets: [
+          {
+            data: [80, 60, 70, 40, 60, 60, 70, 80, 30, 40, 40, 50, 70, 60, 70, 80],
+            backgroundColor: [
+              '#fff',
+              '#fff',
+              '#fff',
+              '#fff',
+              '#fff',
+              '#fff',
+              '#fff',
+              '#8e8e8e',
+              '#8e8e8e',
+              '#8e8e8e',
+              '#8e8e8e',
+              '#8e8e8e',
+              '#8e8e8e',
+              '#8e8e8e'
+            ],
+            barPercentage: 0.4
+          }
+        ]
+      },
+      options: {
+        responsive: false,
+        legend: {
+          display: false
+        },
+        scales: {
+          xAxes: [{
+            display: false,
+            ticks: {
+              min: 0,
+              max: 100,
+              fontSize: 14,
+              fontColor: '#fff'
+            }
+          }],
+          yAxes: [{
+            display: true,
+            ticks: {
+              min: 0,
+              max: 100,
+              fontSize: 14,
+              fontColor: '#fff'
+            }
+          }]
+        }
+      }
+    }
+  },
+  mounted () {
+    this.renderChart(this.data, this.options)
+  }
+}
+</script>
